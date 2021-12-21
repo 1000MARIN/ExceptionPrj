251e10fe2d4d0a7cb2276d31d416b708ab6a77c2
1000marin <leexjin@gmail.com>
2021-12-21 15:45:39 +0900

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int result = 0;
+		result = Calculator.add(3, 4);
+		System.out.printf("add: %d\n", result);
+		result = Calculator.sub(3, 4);
+		System.out.printf("sub: %d\n", result);
+		result = Calculator.multi(3, 4);
+		System.out.printf("multi: %d\n", result);
+		result = Calculator.div(3, 3);
+		System.out.printf("div: %d\n", result);
+		
+	} // end of main
+} // end of Program
---
+++ src/천을_넘는_예외.java
@@ -0,0 +1,8 @@
+
+public class 천을_넘는_예외 extends RuntimeException {
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "입력 값의 합이 1000을 넘는 값을 입력하셨습니다.";
+	}
+}
